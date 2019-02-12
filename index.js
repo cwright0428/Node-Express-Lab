734b9099c84a5e5b1d79e00d6a0b81bedd472595
CJ <CJ@CJs-MacBook.local>
2019-02-12 16:05:10 -0600

    refactoring to async/await

--- index.js
+++ index.js
@@ -67,4 +67,22 @@ server.put("/api/posts/:id", (req, res) => {
     }
   });
 });
+
+
+server.delete('/:id', async (req, res) => {
+  try {
+    const count = await db.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ message: 'Post deleted' });
+    } else {
+      res.status(404).json({ message: 'Post could not be found' });
+    }
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error removing the post',
+    });
+  }
+});
 server.listen(5000, () => console.log("up and at em!"));
